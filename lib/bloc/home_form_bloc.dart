c92219508e4527606cd18b5f4f210c14f8b40166
Hans Bakker <h.bakker@antwebsystems.com>
2020-04-16 17:29:12 +0700

    cleanup , no functional change

--- lib/bloc/home_form_bloc.dart
+++ lib/bloc/home_form_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_form_bloc/flutter_form_bloc.dart';
+import 'package:growerp/models/authenticate.dart';
 import '../services/user_repository.dart';
 import 'authentication/authentication.dart';
 import 'dart:async';
@@ -8,8 +9,8 @@ import 'dart:async';
 class HomeFormBloc extends FormBloc<String, String> {
   final UserRepository userRepository;
   final AuthenticationBloc authenticationBloc;
+  Authenticate authenticate;
   final company = TextFieldBloc();
-  var companyName;
   StreamSubscription authSubscription;
 
   HomeFormBloc({@required this.userRepository,
@@ -28,23 +29,16 @@ class HomeFormBloc extends FormBloc<String, String> {
   @override
   void onLoading() async { // for reload
   try {
-    print("==start loading ====");
     authSubscription = await authenticationBloc.listen((state) {
-      print("==2==is state: $state");
       if (state is AuthenticationAuthenticated) {
-        companyName = (authenticationBloc.state as AuthenticationAuthenticated)
-          .authenticate.company.name;
-        print("==company1==$companyName"); //ok
+        authenticate = 
+          (authenticationBloc.state as AuthenticationAuthenticated)
+          .authenticate;
       }
     });
-    print("==company2==$companyName");
-    if (companyName != null) {
-      company.updateInitialValue(companyName);
-      print("==end company.value: ${company.value.toString()}");
-    }
+    company.updateInitialValue(authenticate.company.name);
     emitLoaded();
-    print("==end loading ====");
-  } catch(e) {
+    } catch(e) {
       emitLoadFailed(failureResponse: "catch, error: $e");
     }
   }
@@ -54,6 +48,5 @@ class HomeFormBloc extends FormBloc<String, String> {
     return super.close();
   }
 }
-
 
 
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,5 +1,3 @@
 export 'package:growerp/widgets/loading_dialog.dart';
 export 'package:growerp/widgets/loading_indicator.dart';
 export 'package:growerp/widgets/notifications.dart';
-export 'package:growerp/widgets/form_button.dart';
-export 'package:growerp/widgets/register_button.dart';
