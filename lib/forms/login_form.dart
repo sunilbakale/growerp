482383fd33eabcb03e94ff85c2725db252c70f0f
Hans Bakker <h.bakker@antwebsystems.com>
2020-08-16 11:30:57 +0700

    split up login screen

--- lib/forms/login_form.dart
+++ lib/forms/login_form.dart
@@ -77,12 +77,6 @@ class _LoginHeaderState extends State<LoginHeader> {
 
   @override
   Widget build(BuildContext context) {
-    final _usernameController = TextEditingController()
-      ..text = authenticate?.user?.name != null
-          ? authenticate.user.name
-          : kReleaseMode ? '' : 'admin@growerp.com';
-    final _passwordController = TextEditingController()
-      ..text = kReleaseMode ? '' : 'qqqqqq9!';
     return MultiBlocListener(
         listeners: [
           BlocListener<AuthBloc, AuthState>(
@@ -103,7 +97,7 @@ class _LoginHeaderState extends State<LoginHeader> {
             }
             if (state is LogginInProgress) {
               HelperFunctions.showMessage(
-                  context, 'Getting company list..', Colors.green);
+                  context, 'Logging in....', Colors.green);
             }
             if (state is LoginError) {
               HelperFunctions.showMessage(
@@ -122,7 +116,6 @@ class _LoginHeaderState extends State<LoginHeader> {
         child: BlocBuilder<AuthBloc, AuthState>(builder: (context, state) {
           if (state is AuthUnauthenticated) {
             authenticate = state.authenticate;
-            _usernameController..text = authenticate?.user?.name;
             companyPartyId = authenticate?.company?.partyId;
             companyName = authenticate?.company?.name;
           }
@@ -136,150 +129,158 @@ class _LoginHeaderState extends State<LoginHeader> {
                   : Company(partyId: companyPartyId);
             }
             if (companyPartyId == null) {
-              return Center(
-                  child: Container(
-                      width: 400,
-                      child: Form(
-                        key: _formKey,
-                        child: Column(
-                          children: <Widget>[
-                            SizedBox(height: 40),
-                            Container(
-                              width: 400,
-                              height: 60,
-                              padding: EdgeInsets.symmetric(horizontal: 10.0),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(25.0),
-                                border: Border.all(
-                                    color: Colors.grey,
-                                    style: BorderStyle.solid,
-                                    width: 0.80),
-                              ),
-                              child: DropdownButton(
-                                key: ValueKey('drop_down'),
-                                underline: SizedBox(), // remove underline
-                                hint: Text('Company'),
-//                                value: _companySelected,
-                                items: companies?.map((item) {
-                                  return DropdownMenuItem<Company>(
-                                    child: Text(item?.name ?? 'Company??'),
-                                    value: item,
-                                  );
-                                })?.toList(),
-                                onChanged: (Company newValue) {
-                                  authenticate.company = newValue;
-                                  BlocProvider.of<AuthBloc>(context)
-                                      .add(UpdateAuth(authenticate));
-                                  Navigator.pushNamedAndRemoveUntil(context,
-                                      HomeRoute, ModalRoute.withName(HomeRoute),
-                                      arguments: "Ecommerce company changed!");
-                                },
-                                isExpanded: true,
-                              ),
-                            ),
-                          ],
-                        ),
-                      )));
+              return changeEcommerceCompany();
             } else {
-              return Center(
-                  child: Container(
-                      width: 400,
-                      child: Form(
-                        key: _formKey,
-                        child: Column(
-                          children: <Widget>[
-                            SizedBox(height: 40),
-                            SizedBox(height: 20),
-                            TextFormField(
-                              key: Key('username'),
-                              decoration:
-                                  InputDecoration(labelText: 'Username'),
-                              controller: _usernameController,
-                              validator: (value) {
-                                if (value.isEmpty)
-                                  return 'Please enter username or email?';
-                                return null;
-                              },
-                            ),
-                            SizedBox(height: 20),
-                            TextFormField(
-                                key: Key('password'),
-                                validator: (value) {
-                                  if (value.isEmpty)
-                                    return 'Please enter your password?';
-                                  return null;
-                                },
-                                controller: _passwordController,
-                                obscureText: _obscureText,
-                                decoration: InputDecoration(
-                                  labelText: 'Password',
-                                  suffixIcon: GestureDetector(
-                                    onTap: () {
-                                      setState(() {
-                                        _obscureText = !_obscureText;
-                                      });
-                                    },
-                                    child: Icon(_obscureText
-                                        ? Icons.visibility
-                                        : Icons.visibility_off),
-                                  ),
-                                )),
-                            SizedBox(height: 20),
-                            RaisedButton(
-                                child: Text('Login'),
-                                onPressed: () {
-                                  if (_formKey.currentState.validate() &&
-                                      state is! LogginInProgress)
-                                    BlocProvider.of<LoginBloc>(context).add(
-                                        LoginButtonPressed(
-                                            companyPartyId:
-                                                _companySelected.partyId,
-                                            username: _usernameController.text,
-                                            password:
-                                                _passwordController.text));
-                                }),
-                            SizedBox(height: 30),
-                            GestureDetector(
-                              child: Text('register new account'),
-                              onTap: () async {
-                                final dynamic result =
-                                    await Navigator.pushNamed(
-                                        context, RegisterRoute);
-                                HelperFunctions.showMessage(
-                                    context, '$result', Colors.green);
-                              },
-                            ),
-                            SizedBox(height: 30),
-                            GestureDetector(
-                                child: Text('forgot password?'),
-                                onTap: () async {
-                                  final String username =
-                                      await _sendResetPasswordDialog(
-                                          context,
-                                          authenticate?.user?.name == null ||
-                                                  kReleaseMode
-                                              ? 'admin@growerp.com'
-                                              : authenticate?.user?.name);
-                                  if (username != null) {
-                                    BlocProvider.of<AuthBloc>(context)
-                                        .add(ResetPassword(username: username));
-                                    HelperFunctions.showMessage(
-                                        context,
-                                        'An email with password has been send to $username',
-                                        Colors.green);
-                                  }
-                                }),
-                            Container(
-                              child: state is LogginInProgress
-                                  ? CircularProgressIndicator()
-                                  : null,
-                            ),
-                          ],
-                        ),
-                      )));
+              return loginToCurrentCompany(state);
             }
           });
         }));
+  }
+
+  Widget changeEcommerceCompany() {
+    return Center(
+        child: Container(
+            width: 400,
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 40),
+                  Container(
+                    width: 400,
+                    height: 60,
+                    padding: EdgeInsets.symmetric(horizontal: 10.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25.0),
+                      border: Border.all(
+                          color: Colors.grey,
+                          style: BorderStyle.solid,
+                          width: 0.80),
+                    ),
+                    child: DropdownButton(
+                      key: ValueKey('drop_down'),
+                      underline: SizedBox(), // remove underline
+                      hint: Text('Company'),
+//                                value: _companySelected,
+                      items: companies?.map((item) {
+                        return DropdownMenuItem<Company>(
+                          child: Text(item?.name ?? 'Company??'),
+                          value: item,
+                        );
+                      })?.toList(),
+                      onChanged: (Company newValue) {
+                        authenticate.company = newValue;
+                        BlocProvider.of<AuthBloc>(context)
+                            .add(UpdateAuth(authenticate));
+                        Navigator.pushNamedAndRemoveUntil(
+                            context, HomeRoute, ModalRoute.withName(HomeRoute),
+                            arguments: "Ecommerce company changed!");
+                      },
+                      isExpanded: true,
+                    ),
+                  ),
+                ],
+              ),
+            )));
+  }
+
+  Widget loginToCurrentCompany(state) {
+    final _usernameController = TextEditingController()
+      ..text = authenticate?.user?.name != null
+          ? authenticate.user.name
+          : kReleaseMode ? '' : 'admin@growerp.com';
+    final _passwordController = TextEditingController()
+      ..text = kReleaseMode ? '' : 'qqqqqq9!';
+    return Center(
+        child: Container(
+            width: 400,
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 40),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    key: Key('username'),
+                    decoration: InputDecoration(labelText: 'Username'),
+                    controller: _usernameController,
+                    validator: (value) {
+                      if (value.isEmpty)
+                        return 'Please enter username or email?';
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 20),
+                  TextFormField(
+                      key: Key('password'),
+                      validator: (value) {
+                        if (value.isEmpty) return 'Please enter your password?';
+                        return null;
+                      },
+                      controller: _passwordController,
+                      obscureText: _obscureText,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        suffixIcon: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              _obscureText = !_obscureText;
+                            });
+                          },
+                          child: Icon(_obscureText
+                              ? Icons.visibility
+                              : Icons.visibility_off),
+                        ),
+                      )),
+                  SizedBox(height: 20),
+                  RaisedButton(
+                      child: Text('Login'),
+                      onPressed: () {
+                        if (_formKey.currentState.validate() &&
+                            state is! LogginInProgress)
+                          BlocProvider.of<LoginBloc>(context).add(
+                              LoginButtonPressed(
+                                  companyPartyId: _companySelected.partyId,
+                                  username: _usernameController.text,
+                                  password: _passwordController.text));
+                      }),
+                  SizedBox(height: 30),
+                  GestureDetector(
+                    child: Text('register new account'),
+                    onTap: () async {
+                      final dynamic result =
+                          await Navigator.pushNamed(context, RegisterRoute);
+                      HelperFunctions.showMessage(
+                          context, '$result', Colors.green);
+                    },
+                  ),
+                  SizedBox(height: 30),
+                  GestureDetector(
+                      child: Text('forgot password?'),
+                      onTap: () async {
+                        final String username = await _sendResetPasswordDialog(
+                            context,
+                            authenticate?.user?.name == null || kReleaseMode
+                                ? 'admin@growerp.com'
+                                : authenticate?.user?.name);
+                        if (username != null) {
+                          BlocProvider.of<AuthBloc>(context)
+                              .add(ResetPassword(username: username));
+                          HelperFunctions.showMessage(
+                              context,
+                              'An email with password has been '
+                              'send to $username',
+                              Colors.green);
+                        }
+                      }),
+                  Container(
+                    child: state is LogginInProgress
+                        ? CircularProgressIndicator()
+                        : null,
+                  ),
+                ],
+              ),
+            )));
   }
 }
 
